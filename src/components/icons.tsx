636e24416682bcceea99bd7641a93714330c199f
apar class <aparclassroom5@gmail.com>
2025-07-03 21:13:59 +0000

    use this every were as my logo .resize the logo as per needed and also t

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,24 +1,17 @@
 import type { SVGProps } from "react";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
+export function Logo(props: React.ComponentPropsWithoutRef<'div'>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M12 2L12 22" />
-      <path d="M2 12L22 12" />
-      <path d="M12 2a10 10 0 0 0-9.95 9.5" />
-      <path d="M2.05 12.5a10 10 0 0 0 9.95 9.5" />
-      <path d="M12 22a10 10 0 0 0 9.95-9.5" />
-      <path d="M21.95 11.5a10 10 0 0 0-9.95-9.5" />
-    </svg>
+    <div {...props} className={cn("relative", props.className)}>
+        <Image
+          src="https://i.ibb.co/6y62gds/image.png"
+          alt="medi assist ai logo"
+          fill
+          sizes="48px"
+        />
+    </div>
   );
 }
 
