23d8f75c2f8002f9589fb2bf5a9a17d635da28c7
apar class <aparclassroom5@gmail.com>
2025-07-03 21:26:04 +0000

    use this logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,7 +5,7 @@ export function Logo(props: React.ComponentPropsWithoutRef<'div'>) {
   return (
     <div {...props}>
         <img
-          src="https://i.ibb.co/Jq6z9zQ/logo.png"
+          src="https://i.ibb.co/wJtW7tZ/logo.png"
           alt="medi assist ai logo"
           className="h-full w-full object-contain"
         />
